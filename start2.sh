ecb7aacd8e067472fc5cbbab954576048776c9c0
David Guzmán <djguzmanc@unal.edu.co>
2017-11-12 17:23:40 -0500

    big commit

---
+++ start2.sh
@@ -0,0 +1,1 @@
+./rancher-compose --project-name upload-ms --url http://192.168.99.100:8080/v1/projects/1a5 --access-key 83C2303F3C1CE12AEBBB --secret-key r6MrtdDxtyNATmbYknhaPXAaXTtypR3okFyVFHXL -f docker-compose.yml --verbose up -d --force-upgrade --confirm-upgrade
