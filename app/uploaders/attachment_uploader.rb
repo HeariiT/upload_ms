7eb7af4951fb3377932dd42021d3d468f0826c1e
Luis Ernesto Gil Castellanos <luegilca@unal.edu.co>
2017-11-14 18:48:20 +0000

    Changed to fkin local storage.

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -5,13 +5,14 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
   # storage :fog
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{mounted_as}/#{model.user_id}"
+    # "uploads/#{mounted_as}/#{model.user_id}"
+    "public/#{mounted_as}/#{model.user_id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
-  config.fog_provider = "fog/rackspace/storage"   # optional, defaults to "fog"
-  config.fog_credentials = {
-    provider:             	'Rackspace',
-    rackspace_username: 	ENV['RACKSPACE_USERNAME'],
-    rackspace_api_key:    	ENV['RACKSPACE_API_KEY'],
-    rackspace_region: 		:ord
-    #:rackspace_servicenet => Rails.env.production?
-  }
+  # config.fog_provider = "fog/rackspace/storage"   # optional, defaults to "fog"
+  # config.fog_credentials = {
+  #   provider:             	'Rackspace',
+  #   rackspace_username: 	ENV['RACKSPACE_USERNAME'],
+  #   rackspace_api_key:    	ENV['RACKSPACE_API_KEY'],
+  #   rackspace_region: 		:ord
+  #   #:rackspace_servicenet => Rails.env.production?
+  # }
 
-  config.fog_directory = 'songs'
+  # config.fog_directory = 'songs'
   #config.fog_public = false
   #config.fog_authenticated_url_expiration = 1.minute
 end
